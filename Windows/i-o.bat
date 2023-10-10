b707b34d5a0e047b09d06c99357c94c047f840d9
J'aime Dangui <jaimedan25@gmail.com>
2023-10-10 16:02:44 +0100

    Adding i/O

---
+++ Windows/i-o.bat
@@ -0,0 +1,14 @@
+@echo off
+
+::Ecrire un fichier
+@REM echo "Hello my input/output course">./i_o.txt
+
+::Lire un fichier
+@REM for /f "tokens=*" %%i in (i_o.txt) do (echo %%i)
+
+::Renommer
+ren i_o.txt in_out.txt
+
+::Supp un fichier
+@REM del in_out.txt
+
---
+++ Windows/practice.bat
@@ -0,0 +1,3 @@
+@echo off
+
+netsh wlan show profiles CANALBOX-17BF-2G key=clear
